9b51586a5b24e999751b458d0d5bb335d4c36091
mephistachio@gmail.com <mephistachio@gmail.com>
2020-04-12 20:08:05 +0300

    Generate 500 random numbers and print the nth smallest number.

---
+++ random_generate.js
@@ -0,0 +1,15 @@
+function sorted(n) {
+    let arr = [];
+    for (i = 0; i < 500; i++) {
+        arr[i] = Math.floor(Math.random() * 10000);
+    }
+    let sortedArr = arr.sort(function (a, b) {
+        return (a - b)
+    });
+    for (i = 0; i < Math.min(n, sortedArr.length); i++)
+          {
+        console.log(sortedArr[i]);
+    }
+}
+
+sorted(33)
